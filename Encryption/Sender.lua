2ba632616f30ac9d55aaa1ce6f47cdfac09255fd
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-20 08:46:19 -0500

    Update Sender.lua (not complete)

--- Encryption/Sender.lua
+++ Encryption/Sender.lua
@@ -1,9 +1,34 @@
 --format: [Destination] [From] [Message]
 --PORT: 1
 --Router address: a88bbfe2-7e88-48a6-9c58-a67e48f07ee9 (testing world)
-local component = require("component")
-local event = require("event")
+component = require("component")
+event = require("event")
 m = component.modem
-local words = {}
-local mainport = 1 --Change to change port, also change top comment
+words = {}
+mainport = 1 --Change to change port, also change top comment
 d = component.data
+serialization = require("serialization")
+router = "a88bbfe2-7e88-48a6-9c58-a67e48f07ee9"
+destination = "" --replace with target, may also make something to automatically set this
+from = "" --replace with device name
+__packet = 
+{
+  header =
+  {
+    sPublic = nil,
+    iv = nil
+  },
+
+  data = nil
+}
+Function MainFunc(receiver, from, port, dist, message)
+  words = {}
+  print("Got a message from " .. from .. " on port " .. port .. ":" .. tostring(message))
+  for w in string.gmatch(tostring(message), "[^ ]+") do
+    table.insert(words, w)
+  end
+  for k, v in pairs(words) do
+    if k == 3 and v == ""
+  end
+end
+event.listen("modem_message", MainFunc)
