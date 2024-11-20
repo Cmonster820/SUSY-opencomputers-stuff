ffe4eface433a83a741607ddd4d95bc65c48ee81
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-20 08:16:21 -0500

    Update Sender.lua with preprocessing stuff for this time

--- Encryption/Sender.lua
+++ Encryption/Sender.lua
@@ -3,6 +3,7 @@
 --Router address: a88bbfe2-7e88-48a6-9c58-a67e48f07ee9 (testing world)
 local component = require("component")
 local event = require("event")
-local m = component.modem
+m = component.modem
 local words = {}
 local mainport = 1 --Change to change port, also change top comment
+d = component.data
