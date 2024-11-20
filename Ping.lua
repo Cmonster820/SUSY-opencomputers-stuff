a12b612e447464c9d7d6d4e6bd58e9f48556ed41
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-20 08:18:38 -0500

    Fix Ping.lua

--- Ping.lua
+++ Ping.lua
@@ -1,14 +1,14 @@
 local component = require("component")
 local event = require("event")
-local m = component.modem
+m = component.modem
 local words = {}
-local mainport = 1 --Change to change port, also change top comment
-local pongs = {"a21d01d1-fefa-4bf5-8af9-77850f43f60c"} --update list to be list of everything to ping
+mainport = 1 --Change to change port, also change top comment
+pongs = {"a21d01d1-fefa-4bf5-8af9-77850f43f60c"} --update list to be list of everything to ping
 m.open(mainport)
 print(m.isOpen(mainport))
-local router = "a88bbfe2-7e88-48a6-9c58-a67e48f07ee9" --change to router's
+router = "a88bbfe2-7e88-48a6-9c58-a67e48f07ee9" --change to router's
 print("router = ", router)
-local mainframe = "5287b40b-8bcb-4bfc-af82-fbd76ce133ed" --change to mainframe modem address
+mainframe = "5287b40b-8bcb-4bfc-af82-fbd76ce133ed" --change to mainframe modem address
 print("mainframe = ", mainframe)
 countervalue = 0
 function detectIssues()
