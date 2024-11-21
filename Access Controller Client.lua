ca362fd336981e0cd1e44b92a8d23f9d2bfaf190
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-21 14:36:48 -0500

    Update Access Controller Client.lua to use packets rather than strings

--- Access Controller Client.lua
+++ Access Controller Client.lua
@@ -1,11 +1,10 @@
---format: [Destination] [From] [Message]
+--format: see __packet and __encryptedpacket
 --PORT: 1
 --Router address: a88bbfe2-7e88-48a6-9c58-a67e48f07ee9 (testing world)
 component = require("component")
 event = require("event")
 m = component.modem
 d = component.data
-words = {}
 mainport = 1 --Change to change port, also change top comment
 door = component.os_doorcontroller
 serialization  = require("serialization")
@@ -57,18 +56,49 @@ Function Handshake()
     print("Handshake [=====] - Key Object Reconstructed, Handshake Complete")
     print("Handshake COMPLETE")
     print("Hands Have Been Shaken")
+    print("Handing off to Encryption Processor")
     return rPublic
 end
 Function EncryptAndSendCardData(cardData)
     rPublic = Handshake()
-    
+    print("Picking Up Encryption Process")
+    print("Encrypting [      ] - Generating Key Pair")
+    local sPublic, sPrivate = d.generateKeyPair(384)
+    print("Encrypting [=     ] - Key Pair Generated, Generating Encryption Key")
+    local encryptionKey = d.md5(d.ecdh(sPrivate, rPublic))
+    print("Encrypting [==    ] - Encryption Key Generated, Generating Initiation Vector")
+    __encryptedpacket.header.iv = d.random(16)
+    print("Encrypting [===   ] - Initiation Vector Generated, Storing sPublic to Packet Header")
+    __encryptedpacket.header.sPublic = sPublic.serialize()
+    print("Encrypting [====  ] - sPublic Stored to Packet Header, Storing cardData to Packet Data")
+    __encryptedpacket.data = cardData
+    print("Encrypting [===== ] - cardData Stored to Packet Data, Serializing and Encrypting Packet Data")
+    __encryptedpacket.data = d.encrypt(serialization.serialize(__encryptedpacket.data), encryptionKey, __encryptedpacket.header.iv)
+    print("Encrypting [======] - Packet Data Encrypted and Serialized")
+    print("Encrypted [======]")
+    print("Constructing and Sending Packet [    ] - Storing Target to Routing Data")
+    __packet.routingData.destination = "mainframe"
+    print("Constructing and Sending Packet [=   ] - Target Stored to Routing Data, Storing Encrypted Packet to Data")
+    __packet.data = __encryptedpacket
+    print("Constructing and Sending Packet [==  ] - Encrypted Packet Stored to Data, Serializing Packet")
+    local message = serialization.serialize(__packet)
+    print("Packet Constructed, Sending Packet [=== ] - Packet Serialized, Sending Packet")
+    m.send(router, mainport, message)
+    print("Sending Packet [====] - Packet Sent")
+    print("Packet Sent")
+    __packet.routingData.destination = nil
+    __packet.data = nil
+    __encryptedpacket.header.iv = nil
+    __encryptedpacket.header.sPublic = nil
+    __encryptedpacket.data = nil
+    print("Packets Reset, Resuming Operation")
+    event.listen("modem_message", pong)
 end
 while true do
     local eventName, address, playerName, cardData, cardUniqueId, isCardLocked, side = event.pull("magData") --takes data from magreader
     EncryptAndSendCardData(cardData)
     print("Authorizing")
     event.ignore("modem_message", pong)
-    words = {}
     local receiver, from, port, dist, message = event.pull("modem_message")
     event.listen("modem_message", pong)
     print("Got a message from " .. from .. " on port " .. port .. ":" .. tostring(message))
