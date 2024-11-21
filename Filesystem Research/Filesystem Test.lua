62fca53556724c423f6863e337f3178b7461db7c
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-21 18:19:03 -0500

    Update Filesystem Test.lua

--- Filesystem Research/Filesystem Test.lua
+++ Filesystem Research/Filesystem Test.lua
@@ -1,6 +1,6 @@
---format: [Destination] [From] [Message] (Message is serialized- change to format made 11/21/24)
+--format: see __packet
 --PORT: 1
---Router address: a88bbfe2-7e88-48a6-9c58-a67e48f07ee9 (testing world)
+--Router address:  (testing world)
 component = require("component")
 event = require("event")
 m = component.modem
@@ -9,6 +9,15 @@ mainport = 1 --Change to change port, also change top comment
 m.open(mainport)
 print(m.isOpen(mainport))
 serialization = require("serialization")
+__packet =
+{
+  routingData =
+  {
+    from = nil
+    destination = nil
+  }
+  data = nil
+}
 if filesystem.exists("/usr/router/") == false then
   filesystem.makeDirectory("/usr/router/")
   names = io.open("/usr/router/names.txt", "a")
@@ -18,63 +27,20 @@ if filesystem.exists("/usr/router/") == false then
 end
 function ProcessRouterCommands(receiver, from, port, dist, message)
   event.ignore("modem_message", MainFunc)
-  words = {}
-  for w in string.gmatch(tostring(message), "[^ ]+") do
-    table.insert(words, w)
   end
-  for k, v in pairs(words) do
-    if k == 1 then
-      local target = v
-    elseif k == 2 then
-      local sender = v
-    elseif k == 3 and v == "newtonetwork" then
-      m.send(from, port, "Transmit Name in 1 second") --name will be sent as the entire message part, not serialized, so splitting is not necessary
-    end
-  end
-  local receiver, secondfrom, port, dist, message = event.pull("modem_message")
-  if secondfrom ~= from then
-    goto 35
-  else
-    local secondfrom = from
-  end
-  local name = message
-  for line in io.lines("/usr/router/names.txt") do
-    if line == name then
-      m.send(from, port, "name taken")
-      goto 36
-    end
-  end
-  io.open("/usr/router/names.txt", "a")
-  names:write("\n" .. tostring(name))
-  names:close()
-  io.open("/usr/router/addresses.txt", "a")
-  addresses:write("\n" .. tostring(from))
-  addresses:close()
-  goto 88
 end
 function MainFunc(receiver, from, port, dist, message)
-  words = {}
-  for w in string.gmatch(tostring(message), "[^ ]+") do
-    table.insert(words, w)
-  end
-  for k, v in pairs(words) do
-    if k == 1 and v == "router" then
-      ProcessRouterCommands(receiver, from, port, dist, message)
-    end
-    if k == 1 then
-      local simpletarget = v
-      for line in io.lines("/usr/names.txt") do
-        if line == v then
-          local lineinaddresses = k
-          names:close()
-        end
+  local message = serialization.unserialize(message)
+  io.open("/usr/router/names.txt", "r")
+  local n = 1
+    for line in io.lines("/usr/router/names.txt") do
+      local n = n+1
+      if line == message.routingData.from then
+        local lineinaddresses = n
+        names:close()
       end
-    elseif k == 2 then
-      local simplefrom = v
-    elseif k == 3 then
-      local data = v
     end
-  end
+  io.open("/usr/router/addresses.txt", "r")
   local n = 1
   for line in io.lines("/usr/addresses.txt") do
     local n = n+1
