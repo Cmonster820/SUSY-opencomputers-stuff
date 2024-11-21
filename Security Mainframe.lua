a3f6ad4d0173b8cd881f553ccd2dfeb322a975ae
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-21 15:15:32 -0500

    Update Security Mainframe.lua (not done yet)

--- Security Mainframe.lua
+++ Security Mainframe.lua
@@ -1,56 +1,73 @@
---format: [Destination] [From] [Message]
+--format: see __packet and __encryptedpacket
 --PORT: 1
 --Router address: a88bbfe2-7e88-48a6-9c58-a67e48f07ee9 (testing world)
 component = require("component")
 event = require("event")
 m = component.modem
-words = {}
+d = component.data
 mainport = 1 --Change to change port, also change top comment
-Cards = {"123"}
+door = component.os_doorcontroller
+serialization  = require("serialization")
+name = nil --change this to from a file later
+sPublic = nil
+sPrivate = nil
+rPublic = nil
+rPrivate = nil
+__packet =
+{
+    routingData =
+    {
+        destination = nil
+        from = name
+    }
+    data = nil
+}
+__encryptedpacket = 
+{
+    header =
+    {
+        iv = nil
+        sPublic = nil
+    }
+    data = nil
+}
+function ReceiveAndDecrypt()
+    local receiver, from, port, distance, message = event.pull("modem_message")
+    print("Encrypted Message Received \n Decrypting Message [       ] - Unserializing Packet")
+    local message = serialization.unserialize(message)
+    print("Decrypting Message [=      ] - Packet Unserialized, Deserializing sPublic")
+    sPublic = d.deserializeKey(message.data.__encryptedpacket.header.sPublic,"ec-public")
+    print("Decrypting Message [==     ] - sPublic Deserialized, Generating Decryption Key")
+    local decryptionKey = d.md5(d.ecdh(rPrivate, sPublic))
+    print("Decrypting Message [===    ] - Decryption Key Generated, Decrypting Message")
+    local data = component.data.decrypt(message.data.__encryptedpacket.data, decryptionKey, message.data.__encryptedpacket.header.iv)
+    local message = serialization.unserialize(data)
+end
+function RespondToHandshake(receiver, from, port, distance, message)
+    event.ignore("modem_message", MainFunc)
+    print("Responding to Handshake [    ] - Storing Routing Data")
+    __packet.routingData.destination = message.routingData.from
+    print("Responding to Handshake [=   ] - Routing Data Stored, Generating Key Pair")
+    rPublic, rPrivate = d.generateKeyPair(384)
+    print("Responding to Handshake [==  ] - Key Pair Generated, Serializing rPublic and Storing to Packet Data")
+    __packet.data = rPublic.serialize()
+    print("Responding to Handshake [=== ] - rPublic Serialized and Stored to Packet Data, Serializing and Sending Packet")
+    m.send(from, port, serialization.serialize(__packet))
+    print("Responding to Handshake [====] - Packet Serialized and Sent")
+    print("Hands Have Been Shaken")
+    __packet.routingData.destination = nil
+    __packet.data = nil
+    print("Awaiting Encrypted Message")
+    ReceiveAndDecrypt()
+end
+function MainFunc(receiver, from, port, distance, message)
+    local message = serialization.unserialize(message)
+    if message.data = "prepare" then
+        RespondToHandshake(receiver, from, port, distance, message)
+    end
+end
 m.open(mainport)
 print(m.isOpen(mainport))
 router = "a88bbfe2-7e88-48a6-9c58-a67e48f07ee9" --change to router's
-print("router = ", router)
-Function AlarmManager(lockdown)
-    if lockdown == true then
-        m.broadcast(mainport, LOCKDOWNBEGIN)
-    elseif lockdown == false then
-        m.broadcast(mainport, ALLCLEAR)
-    end
-end
-Function DoorManager(from, card)
-    for h, j in pairs(Cards) do
-        if card == j then
-            m.send(router, mainport, tostring(from) .. " mainframe authorized")
-        elseif card ~= j and h == #Cards then
-            m.send(router, mainport, tostring(from) .. " mainframe denied")
-        end
-    end
-end
-Function RequestManager(requestType,from,data)
-    if requestType == "open" then
-        DoorManager(from,data)
-    end
-end
-Function MainFunc(receiver, sender, port, dist, message)
-    os.sleep(0.06)
-    words = {}
-    print("Got a message from " .. from .. " on port " .. port .. ":" .. tostring(message))
-    if message == ALARM or message == LOCKDOWN then
-        AlarmManager(true)
-    end
-    for w in string.gmatch(message, "[^ ]+") do --splits the message by word into table words
-        table.insert(words, w)
-    end
-    for k, v in pairs(words) do --looks through words
-        print(k, v)
-        if k == 2 then
-            from = v
-        elseif k == 3 and v == "openrequest" then
-            requestType == "openDoor"
-        elseif k == 4 and requestType == "openDoor" then
-            RequestManager("open",from,v)
-        end
-    end
-end
+print("router =", router)
 event.listen("modem_message", MainFunc)
