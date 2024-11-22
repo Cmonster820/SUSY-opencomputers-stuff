809eae0692c1973d7f604a903fbc5fbb214e8dfa
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-21 19:51:15 -0500

    Update Filesystem Test.lua

--- Filesystem Research/Filesystem Test.lua
+++ Filesystem Research/Filesystem Test.lua
@@ -25,12 +25,36 @@ if filesystem.exists("/home/router/") == false then
   addresses = io.open("user/router/addresses.txt", "a")
   addresses:close()
 end
+function AddDeviceToNetwork(receiver, from, port, dist, message)
+  m.send(from, port, "send name in 0.25 seconds")
+  local _, receiver, from, port, dist, message = event.pull("modem_message")
+  for line in io.lines("/home/router/names.txt") do
+    if message == line then
+      m.send(from, port, "name taken")
+      goto 29
+    end
+  end
+  names = io.open("/home/router/names.txt", "a")
+  names:write(message .. "\n")
+  names:close()
+  addresses = io.open("/home/router/addresses.txt", "a")
+  addresses:write(from .. "\n")
+end
 function ProcessRouterCommands(receiver, from, port, dist, message)
   event.ignore("modem_message", MainFunc)
+  if message == "LOCKDOWN" or message == "ALARM" then
+    for line in io.lines("/home/router/addresses.txt") do
+      m.send(line, mainport, "ALARM")
+    end
+  elseif message == "newtonetwork" then
+    AddDeviceToNetwork(receiver, from, port, dist, message)
   end
 end
 function MainFunc(_, receiver, from, port, dist, message)
   local message = serialization.unserialize(message)
+  if message.routingData.from == "router" then
+    ProcessRouterCommands(receiver, from, port, dist, message)
+  end
   local n = 1
     for line in io.lines("/home/router/names.txt") do
       local n = n+1
