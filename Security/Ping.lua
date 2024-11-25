bb21155cff5ce9286f030d1bfc1396560e87997b
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-25 10:08:24 -0500

    try to figure out why my spaghetti code is turning into lasagna

--- Security/Ping.lua
+++ Security/Ping.lua
@@ -1,7 +1,7 @@
 component = require("component")
 event = require("event")
 m = component.modem
-tempongs = {}
+tempongs = {} -- temporary table created from, actually I don't know why this is here
 serialization = require("serialization")
 fs = require("filesystem")
 mainport = 1 --Change to change port, also change top comment (you can tell I used copy and paste here because there is no top comment
