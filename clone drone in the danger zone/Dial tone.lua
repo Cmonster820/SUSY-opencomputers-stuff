462444dbb8750c15b9e3c0b072ca37101134d9ec
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2024-11-26 09:30:18 -0500

    Im going insane

--- clone drone in the danger zone/Dial tone.lua
+++ clone drone in the danger zone/Dial tone.lua
@@ -1,3 +1,4 @@
+--swarm uses old message system due to lack of serialization
 m = component.proxy(component.list("modem"))
 d = component.proxy(component.list("drone"))
 c = component.proxy(component.list("computer"))
@@ -40,3 +41,27 @@ while homex ~= 0 and homez ~= 0 do
     d.move(homex, 0, homez)
 end
 d.setLightColor(0x00FF00)
+name = "drone1"
+m.open(3)
+m.broadcast(3, "addtonetwork")
+function ProcessMessages(receiver, from, port, dist, message)
+    words = {}
+    if message == "addtonetwork" then
+    end
+    for w in string.gmatch(message, "[^ ]+") do
+        table.insert(words, w)
+    end
+    for k, v in pairs(words) do
+        if then
+        end
+    end
+end
+while true do
+    local name = nil
+    local name, dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8 = c.pullSignal(0.25)
+    if name ~= nil then
+        if name == "modem_message" then
+            ProcessMessages(dat1, dat2, dat3, dat4, dat5)
+        end
+    end
+end
