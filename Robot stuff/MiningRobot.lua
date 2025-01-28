ee5f360d1c0a2cce21f0971a65ab5c882d37d2b1
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-28 14:47:33 -0500

    incomplete

--- Robot stuff/MiningRobot.lua
+++ Robot stuff/MiningRobot.lua
@@ -2,6 +2,13 @@ component = require("component")
 event = require("event")
 m = component.modem
 c = component.computer
-nav = component.navigation
+n = component.navigation
 fs = require("filesystem")
 miningNetPort = 5
+db = component.database
+invcont = component.inventory_controller
+r = require("robot")
+waypoints = n.findwaypoints(100)
+for k, v in pairs(waypoint) do
+    if v.label == "miningref" then
+        
