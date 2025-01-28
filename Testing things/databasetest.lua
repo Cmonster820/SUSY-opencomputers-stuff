e5f36133ec483ed18ded68adbd9aedaf45cfb8f1
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-28 09:44:42 -0500

    create dbtest

---
+++ Testing things/databasetest.lua
@@ -0,0 +1,8 @@
+component = require("component")
+event = require("event")
+m = component.modem
+db = component.database
+sides = require("sides")
+invcont = component.inventory_controller
+dbSlot = 1
+invSlot = 1
