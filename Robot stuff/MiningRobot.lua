9b289884401e584a0d3407762180d9bee0165e3d
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-28 17:10:20 -0500

    incomplete

--- Robot stuff/MiningRobot.lua
+++ Robot stuff/MiningRobot.lua
@@ -1,3 +1,4 @@
+--robot MUST be placed facing forwards
 component = require("component")
 event = require("event")
 m = component.modem
@@ -8,7 +9,55 @@ miningNetPort = 5
 db = component.database
 invcont = component.inventory_controller
 r = require("robot")
+sides = require("sides")
 waypoints = n.findwaypoints(100)
 for k, v in pairs(waypoint) do
     if v.label == "miningref" then
-        
+        for l, b in v.position do
+            if l == 1 then
+                refx = -1*b
+            elseif l == 2 then
+                refy = -1*b
+            elseif l == 3 then
+                refz = -1*b
+            end
+        end
+        break
+    end
+end
+function move(x, y, z, rel)
+    if rel == true then
+
+    end
+    if x > 0 then
+        for i, x, 1 do
+            robot.forward()
+        end
+    elseif x < 0 then
+        for i, math.abs(x), 1 do
+            robot.back()
+        end
+    end
+    if y > 0 then
+        for i, y, 1 do
+            robot.up()
+        end
+    elseif y < 0 then
+        for i, math.abs(y), 1 do
+            robot.down()
+        end
+    end
+    if z > 0 then
+        robot.turnRight()
+        for i, z, 1 do
+            robot.forward()
+        end
+        robot.turnLeft()
+    elseif z < 0 then
+        robot.turnLeft()
+        for i, math.abs(z), 1 do
+            robot.forward()
+        end
+        robot.turnRight()
+    end
+end
