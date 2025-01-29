b86d117ef897ba1b6f30b2251cadd0159222010f
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-29 14:50:29 -0500

    incomplete

--- Testing things/Windowslib.lua
+++ Testing things/Windowslib.lua
@@ -8,7 +8,7 @@ g.fill(1,1,g.maxResolution(),"█")]]
 g.fill(1,1,g.maxResolution()," ")
 window = {
     label = "",
-    barcol = nil,
+    barcol = nil, -- color of the bar at the top of the window
     position = {
         x = nil,
         y = nil,
@@ -22,27 +22,45 @@ window = {
         offsetx = nil,
         offsety = nil,
         color = nil
+    },
+    closebutton = {
+        x = math.abs(self.position.w-self.position.x)-1
+        y = self.position.y
     }
 }
 function window:new(o, label, barcol, x, y, w, h, wincol, text, toffsetx, toffsety, textcol)
     o = o or {}
     o.label = label
-    o.barcol = barcol -- color of the bar at the top of the window
+    o.barcol = barcol 
     o.position.x = x
     o.position.y = y
     o.position.w = w
     o.position.h = h
-    o.color = wincol
     o.text.text = text
     o.text.offsetx = toffsetx
+    o.text.offsety = toffsety
     if wincol == nil then
-        local oldfg, _ = gpu.getForeground()
-        gpu.setForeground()
+        wincol = 0xFFFFFF
+    end
+    o.color = wincol
+    if barcol == nil then
+        barcol = 0x5793f2
+    end
+    o.barcol = barcol
+    if textcol == nil then
+        textcol = 0xFFFFFF
+    end
+    o.text.color = textcol
+    local oldfg, _ = g.getForeground()
+    local oldbg, _ = g.getBackground()
+    g.setForeground(o.color) --makes next operation the right color
+    g.fill(o.position.x, o.position.y, o.position.w, o.position.h, o.char) --makes window box
+    g.setForeground(o.barcol) --makes bar the right color
+    g.fill(o.position.x, o.position.y, o.position.w, 1, o.char) --makes bar at top
+    g.setForeground(0xFFFFFF)
+    g.setBackground(o.barcol) -- hopefully this works
+    g.set(o.position.x+1, o.position.y, o.label)
+    g.setBackground(oldbg) --returns background to original color
 
     return o
-end
-function closewindow(win)
-    g.fill(win.position.x,win.position.y,win.position.w,win.position.h,"█")
-    local win = nil
-    return win
 end
