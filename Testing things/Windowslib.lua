c1070bf7462b086839ee7af3ce388b2c6973e359
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-29 12:35:19 -0500

    █

--- Testing things/Windowslib.lua
+++ Testing things/Windowslib.lua
@@ -1,3 +1,15 @@
+--[[ This library is intended for use by myself to be able to 
+create "windows," which can display text in them and stuff]]
 component = require("component")
-g=component.gpu
-__window
+g = component.gpu
+g.setBackground(0xFFFFFF) -- While I can't draw different color rectangles I can cut holes in a white one
+g.setForeground(0x000000)
+window = {
+    position = {
+        x = nil,
+        y = nil,
+        w = nil,
+        h = nil
+    },
+    char = " "
+}
