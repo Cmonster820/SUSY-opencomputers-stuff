b1b569033b245c71cb5b1496522c1a18232a069b
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-01-30 08:19:45 -0500

    realize my mistakes

---
+++ Testing things/GuiLib.lua
@@ -0,0 +1,25 @@
+--[[ This library is intended for use by myself to be able to 
+create guis which can display text in them and stuff]]
+component = require("component")
+event = require("event")
+g = component.gpu
+g.setBackground(0x000000) 
+g.setForeground(0xFFFFFF)
+--[[g.fill(1,1,g.maxResolution(),"█")]]
+g.fill(1,1,g.maxResolution()," ") -- refresh screen
+button = {
+    position = {
+        x = nil,
+        y = nil,
+        w = nil,
+        h = nil
+    },
+    color = nil,
+    char = "█",
+    text = {
+        color = nil,
+        text = nil,
+        x = math.abs(self.position.x-self.position.w)-string.len(self.text.text)/2,
+        y = math.abs(self.position.y-self.position.h),
+    }
+}
