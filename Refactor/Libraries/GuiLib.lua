b99dec39849dea11a87fdb15c595db6fd87852d4
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-10-02 10:11:47 -0400

    no time

--- Refactor/Libraries/GuiLib.lua
+++ Refactor/Libraries/GuiLib.lua
@@ -4,6 +4,7 @@ create guis which can display text in them and stuff
 --I remade it because I really don't want to look through the old library and fix it
 component = require("component")
 event = require("event")
+math = require("math")
 g = component.gpu
 g.setBackground(0x000000) 
 g.setForeground(0xFFFFFF)
@@ -12,7 +13,37 @@ g.fill(1,1,g.maxResolution()," ") -- refresh screen
 g.setDepth(g.maxDepth())
 gauge =
 {
-    
+    position =
+    {
+        x = 0,
+        y = 0,
+        w = 0,
+        h = 0
+    },
+    label = "",
+    readOut = "",
+    fillLvl = 0,
+    fillCol = 0,
+    emptyCol = 0,
+    textCol = 0,
+    optimalEnabled = false,
+    optimalLvl = 0,
+    optimalCol = 0,
+    rendererdata =
+    {
+        fillbar =
+        {
+            x = self.position.x
+            y = self.position.y
+            w = math.floor(self.position.w/fillLvl)
+            h = self.position.h
+        },
+        labelData =
+        {
+            x = (self.position.x+((1/2)*self.position.w)-((1/2)*string.len(self.label)))
+            
+        }
+    }
 }
 button = 
 {
