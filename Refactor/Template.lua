ed12be9b83200b31e0a472ce9d365e659a7a867d
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-10-08 14:25:15 -0400

    not done

---
+++ Refactor/Template.lua
@@ -0,0 +1,47 @@
+component = require("component")
+event = require("event")
+m = component.modem
+filesystem = require("filesystem")
+serialization = require("serialization")
+io = require("io")
+words = {}
+mainport = 1
+m.open(mainport)
+newdeviceport = 2
+m.open(newdeviceport)
+negotiationport = 3
+m.open(negotiationport)
+g = component.gpu
+gui = require("GuiLib")
+if (m.isOpen(mainport) && m.isOpen(newdeviceport) && m.isOpen(negotiationport))=true then
+    print("All ports opened successfully, proceeding with bootup")
+else
+    io.stderr:write("Error detected, halting operation")
+    os.exit()
+end
+packet = 
+{
+    routingData = 
+    {
+        destination = nil,
+        from = nil,
+        fromaddr = nil
+    },
+    data = nil
+}
+resX, resY = g.getResolution
+loadingScreen = gui.gauge:new(resX//4, (resY//2)-1, (3*resX)//4, 3, "Setup", _, "Creating Data Files-Names", _, 0, _, _, _, _, _)
+if filesystem.exists("/home/data") == false then
+    filesystem.makeDirectory("/home/router/")
+    names = io.open("/home/router/names.txt", "a")
+    loadingScreen:refresh(33, "Names File Created, Creating Addresses File", _, _)
+    names:close
+    addresses = io.open("/home/router/addresses.txt", "a")
+    loadingScreen:refresh(67, "Names File Created, Creating Addresses File", _, _)
+    addresses:close
+    log = io.open("/home/router/log.txt", "a")
+    loadingScreen:refresh(100, "Log File Created, Setup Complete", _, _)
+    log:close
+    g.fill(0, 0, g.getResolution(), " ")
+    LoadingScreen = nil
+end
