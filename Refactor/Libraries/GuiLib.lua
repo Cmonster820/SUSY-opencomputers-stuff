638dddeef77c5528287657831fe992c073ec998a
Cmonster820 <72608260+Cmonster820@users.noreply.github.com>
2025-11-13 09:53:43 -0500

    might be done

--- Refactor/Libraries/GuiLib.lua
+++ Refactor/Libraries/GuiLib.lua
@@ -143,4 +143,27 @@ function main.isinsidepoly(polylist, coords)
     if not ((coords[1]<=maxX and coords[1]>=minX) and (coords[2]<=maxY and coords[2]>=minY)) then
         return false
     end
+    for i = 1, #polylist do
+        maxY = -500
+        minY = 500
+        maxX = -1000
+        minX = 1000
+        for j = 1, #polylist[i].points do
+            if polylist[i].points[2]>maxY then
+                maxY = polylist[i].points[2]
+            end
+            if polylist[i].points[2]<minY then
+                minY = polylist[i].points[2]
+            end
+            if polylist[i].points[1]>maxX then
+                maxX = polylist[i].points[1]
+            end
+            if polylist[i].points[1]<minX then
+                minX = polylist[i].points[1]
+            end
+            if (coords[1]<=maxX and coords[1]>=minX) and (coords[2]<=maxY and coords[2]>=minY) then
+                return i
+            end
+        end
+    end
 end
