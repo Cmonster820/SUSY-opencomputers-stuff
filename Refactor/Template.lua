e94e9f7e10c53681fc303b9df853de5f698cb053
Cooper Howard <72608260+Cmonster820@users.noreply.github.com>
2026-02-24 12:28:55 -0500

    update template to use a .csv file as well

--- Refactor/Template.lua
+++ Refactor/Template.lua
@@ -5,7 +5,6 @@ m = component.modem
 filesystem = require("filesystem")
 serialization = require("serialization")
 io = require("io")
-words = {}
 mainport = 1
 m.open(mainport)
 newdeviceport = 2
@@ -13,13 +12,8 @@ m.open(newdeviceport)
 negotiationport = 3
 m.open(negotiationport)
 g = component.gpu
-gui = require("GuiLib")
-if (m.isOpen(mainport) && m.isOpen(newdeviceport) && m.isOpen(negotiationport))=true then
-    print("All ports opened successfully, proceeding with bootup")
-else
-    io.stderr:write("Error detected, halting operation")
-    os.exit()
-end
+assert((m.isOpen(mainport) && m.isOpen(newdeviceport) && m.isOpen(negotiationport))=true,"Error detected, halting operation")
+print("All ports opened successfully, proceeding with bootup")
 packet = 
 {
     routingData = 
@@ -30,58 +24,36 @@ packet =
     },
     data = nil
 }
-name = io.read("/home/data/name.txt", "*l")
 resX, resY = g.getResolution
 if filesystem.exists("/home/data") == false then
-    loadingScreen = gui.gauge:new(resX//4, (resY//2)-1, (3*resX)//4, 3, "Setup", _, "Creating Data Files-Names", _, 0, _, _, _, _, _)
     filesystem.makeDirectory("/home/data/")
-    names = io.open("/home/data/names.txt", "a")
-    loadingScreen:refresh(33, "Names File Created, Creating Addresses File", _, _)
-    names:close()
-    addresses = io.open("/home/data/addresses.txt", "a")
-    loadingScreen:refresh(67, "Addresses File Created, Creating Log File", _, _)
-    addresses:close()
+    ownName = io.open("/home/data/name.txt","a")
+    data = io.open("/home/data/data.csv", "a")
+    data:close()
     log = io.open("/home/data/log.txt", "a")
-    loadingScreen:refresh(100, "Log File Created, Setup Complete", _, _)
     log:close()
-    g.fill(0, 0, g.getResolution(), " ")
-    LoadingScreen = nil
-end
-dataTable = {}
-curline = 1
-for line in io.lines("/home/data/names.txt") do
-    local namel = line:gsub("\n", "")
-    local othercurline = 1
-    for otherline in io.lines("/home/data/addresses.txt") do
-        address = otherline:gsub("\n", "")
-        if curline == othercurline then
-            dataTable[namel] = address
-            break
-        end
-        othercurline++
+else
+    dataCache = {}
+    for line in io.lines("/home/data/data.csv") do
+        commaIndex = line:find(",")
+        dataCache[line:sub(1,commaIndex-1)] = line:sub(commaIndex+1)
     end
-    othercurline = nil
-    curline++
 end
-curline = nil
 function negotiate()
-    names = io.open("/home/data/names.txt","a")
-    addresses = io.open("/home/data/addresses.txt","a")
-    namefile = io.open("/home/data/name.txt","a")
-    log = io.open("/home/data/log.txt", "a")
+    local data = io.open("/home/data/data.csv","a") 
+    local namefile = io.open("/home/data/name.txt","a")
+    local log = io.open("/home/data/log.txt", "a")
     packet.from = name
     m.broadcast(newdeviceport, serialization.serialize(packet))
     log:write("Message broadcasted:\n"+serialization.serialize(packet)+"\n\n\n")
     local receiveraddr, sender, port, distance, message = event.pull("modem_message")
     log:write("Message received from "+sender+" on port "+port+" message reads:\n"+serialization.deserialize(message)+"\n\n\n")
-    names:write("router\n")
-    addresses:write(sender+"\n")
+    data:write("router",sender)
     if message == "Negotiation Successful" then
         print("Negotiation Successful")
         log:close()
-        names:close()
+        data:close()
         namefile:close()
-        addresses:close()
     elseif message == "Name Taken" then
         --[[
         io.stderr:write("Error: name taken, halting operation")
@@ -101,18 +73,17 @@ function negotiate()
             taken = not(serialization.deserialize(message)=="Name Taken")
         end
         log:close()
-        addresses:close()
+        data:close()
         namefile:close()
         filesystem.remove("/home/data/name.txt")
         namefile = io.open("/home/data/name.txt", "a")
         namefile:write(name)
         namefile:close()
-        names:close()
         print("Negotiation Complete")
         return nil]]
     end
 end
-if dataTable[router] == nil then
+if dataCache[router] == nil then
     negotiate()
 end
 function mainfunction(receiveraddr, sender, port, distance, message) --rename if needed
